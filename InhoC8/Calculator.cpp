0b19fd8c79098e6dcd397a5932841e3b42a14ebf
jeoninho623 <0623inho@naver.com>
2023-03-12 10:04:27 +0900

    계산기 두가자

---
+++ InhoC8/Calculator.cpp
@@ -0,0 +1,40 @@
+﻿// InhoC8.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "framework.h"
+#include "InhoC8.h"
+
+INT_PTR CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDCANCEL)
+        {
+            // 취소버튼을 누르면 동작할 코드
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        else if (LOWORD(wParam) == IDOK) {
+            // 확인버튼을 누르면 동작할 코드
+            MessageBox(hDlg,L"확인버튼 눌림",L"제목",MB_OK);
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+   
+    DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc);
+    return 0;
+}
+
+
